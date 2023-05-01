6a6ba1d8fa3f5ea8f30228940a8ba850411d6280
Zanaya Austin <zanayaaustin427@gmail.com>
2023-05-01 11:42:42 -0400

    SF intial commit

---
+++ exercises/functions/simple_functions.js
@@ -0,0 +1,16 @@
+function favoriteThings() {
+console.log("Zanaya's favorite things are: ");
+console.log("My favorite food is Seafood Boil. ");
+console.log("My favorite sport is Baskeball. ");
+}
+
+favoriteThings();
+
+function whyImHere() {
+    console.log(
+        `I really like the environment and the
+        motivation we constantly push each other.`
+);
+}
+
+whyImHere();
