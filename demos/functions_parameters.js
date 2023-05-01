5e58c6f483d684e50fc3b97a4302575e55a82a04
Zanaya Austin <zanayaaustin427@gmail.com>
2023-05-01 12:29:14 -0400

    parameter pt 1

---
+++ demos/functions_parameters.js
@@ -0,0 +1,24 @@
+//function receives the parameter
+function display(message){
+console.log(message);
+}
+
+// call function pass argument
+display ("A given message");
+display ("I'm getting hungry")
+display("When am I eating seafood boil again")
+
+let myMessage = "test this out";
+print(myMessage);
+
+let basketball = "basketball";
+
+function shoot(ball) {
+    console.log("Score! 3-pointer.");
+}else{
+    console.log("You lose");
+}
+
+
+let basketball = "basketball";
+shoot(basketball);
